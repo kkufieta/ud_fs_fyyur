7c0b9f120874c2f9a5b07dad5f825ac27e5d9d85
kkufieta <katharina.kufieta@gmail.com>
2020-01-10 16:44:28 -0500

    Implement create show endpoint

--- app.py
+++ app.py
@@ -425,13 +425,24 @@ def create_shows():
 @app.route('/shows/create', methods=['POST'])
 def create_show_submission():
   # called to create new shows in the db, upon submitting new show listing form
-  # TODO: insert form data as a new Show record in the db, instead
+  try:
+    data = request.form
+    print(data)
+    show = Show(artist_id=data['artist_id'],
+                venue_id=data['venue_id'],
+                start_time=data['start_time'])
+    db.session.add(show)
+    db.session.commit()
+    # Flashing: http://flask.pocoo.org/docs/1.0/patterns/flashing/
+    flash('Show was successfully listed!')
+  except:
+    error = True
+    db.session.rollback()
+    print(sys.exc_info())
+    flash('An error occured. Show could not be listed.')
+  finally:
+    db.session.close()
 
-  # on successful db insert, flash success
-  flash('Show was successfully listed!')
-  # TODO: on unsuccessful db insert, flash an error instead.
-  # e.g., flash('An error occurred. Show could not be listed.')
-  # see: http://flask.pocoo.org/docs/1.0/patterns/flashing/
   return render_template('pages/home.html')
 
 @app.errorhandler(404)
